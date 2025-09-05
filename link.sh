cd5c6563cebbb65f999c8d85879141238dde1685
Yura Dupyn <2153100+omedusyo@users.noreply.github.com>
2025-09-05 14:21:44 +0200

    Update links

--- link.sh
+++ link.sh
@@ -6,5 +6,7 @@ ln -fs ~/kosmos/dotfiles/nvim ~/.config/nvim
 ln -fs ~/kosmos/dotfiles/kitty ~/.config/kitty
 ln -fs ~/kosmos/dotfiles/zellij ~/.config/zellij
 ln -fs ~/kosmos/dotfiles/yazi ~/.config/yazi
-
+ln -fs ~/kosmos/dotfiles/zathura ~/.config/zathura
 ln -fs ~/kosmos/dotfiles/kritaconfig/kritashortcutsrc ~/.config/kritashortcutsrc
+
+ln -fs ~/kosmos/dotfiles/cursor/mcp.json ~/.cursor/mcp.json
